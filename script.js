b8570d53f65b4ee2de4423ff8cd4a2fa67eaf1cf
Jawid Rahimi <j.rahimi.1@student.rug.nl>
2025-03-24 17:54:57 +0100

    - fixed homepage a little - fixed dropdown navbar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-    const menu = document.getElementById('dropdown-menu'); // Assuming you have a dropdown menu
-    menu.classList.toggle('show'); // Toggles the 'show' class to display/hide the menu
+    const menu = document.getElementById('dropdown-menu');
+    menu.classList.toggle('show');
 }
