4329eabff5177ff4b9e84128e4e0f701884f7908
Jawid Rahimi <j.rahimi.1@student.rug.nl>
2025-03-26 12:54:02 +0100

    - Fixed homepage, search interface functional

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const menu = document.getElementById('dropdown-menu');
     menu.classList.toggle('show');
 }
+
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        window.location.href = 'search_results.html';
+    }
+});
+
+document.getElementById('searchButton').addEventListener('click', function(){
+    window.location.href = 'search_results.html';
+});
